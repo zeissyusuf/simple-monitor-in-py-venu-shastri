32cc8dea37dda4380c6900c83a1df0d29c4ec0f8
zyucc <110530125+zeissyusuf@users.noreply.github.com>
2022-09-09 11:53:04 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -3,21 +3,21 @@ def check_temp(temperature):
   if temperature < 0 or temperature > 45:
     ez= False
     print('Temperature is out of range!')
-   return ez
+ return ez
 
 def check_soc(soc):
   ez = True
   if soc < 20 or soc > 80:
     ez = False
     print('State of Charge is out of range!')
-   return ez
+  return ez
 
 def check_cr(charge_rate):
   ez = True
   if charge_rate > 0.8:
     ez = False
     print('Charge rate is out of range!')
-   return ez
+  return ez
 
 def battery_is_ok(temperature, soc, charge_rate):
   onebool = check_temp(temperature)
